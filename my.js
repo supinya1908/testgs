7aaca28c500613e894c1a1b9ec239b5df74e05ac
supinya1908 <37093123+supinya1908@users.noreply.github.com>
2020-08-20 15:54:36 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,17 +1,34 @@
 
+
 function testGS() {
 
     const url = "https://script.google.com/macros/s/AKfycbwiCOhXQp7LgZw352TvEoejfGxJjA-pWsK88avG_WX8a9iurwn8/exec";
 
-
     fetch(url)
         .then(d => d.json())
         .then(d => {
-
             document.getElementById("app").textContent = d[0].status;
-
         });
+}
+
+function addGS() {
+
+    const url = "https://script.google.com/macros/s/AKfycbwiCOhXQp7LgZw352TvEoejfGxJjA-pWsK88avG_WX8a9iurwn8/exec";
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"John"}) // body data type must match "Content-Type" header
+      });
 
 }
+
+document.getElementById("btn2").addEventListener("click",addGS);
 
 document.getElementById("btn").addEventListener("click",testGS);
