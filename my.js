8c32eee0c37536ed7ba62b4a8981d37110eda9c3
supinya1908 <37093123+supinya1908@users.noreply.github.com>
2020-08-22 20:07:38 +0700

    Update my.js

--- my.js
+++ my.js
@@ -24,7 +24,7 @@ function addGS() {
         },
         redirect: 'follow', // manual, *follow, error
         //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({first:"Momoko",phone:"081-668-3441",last:"zaq"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({name:"Johon"}) // body data type must match "Content-Type" header
       });
 
 }
